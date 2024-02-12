fa65c7fb31544357e2cf6223a815908a70e8c1a6
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-02-12 16:53:24 -0500

    Add basic tests for handler

---
+++ handler/handler_test.go
@@ -0,0 +1,122 @@
+package handler
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type fakeIataFinder struct {
+	iata  string
+	err   error
+	loads int
+}
+
+func (f *fakeIataFinder) Lookup(country string, lat, lon float64) (string, error) {
+	if f.err != nil {
+		return "", f.err
+	}
+	return f.iata, nil
+}
+func (f *fakeIataFinder) Load(ctx context.Context) error {
+	f.loads++
+	return nil
+}
+
+func TestServer_Lookup(t *testing.T) {
+	tests := []struct {
+		name     string
+		project  string
+		iata     *fakeIataFinder
+		request  string
+		headers  map[string]string
+		wantCode int
+		wantIata string
+	}{
+		{
+			name:     "success-parameters",
+			iata:     &fakeIataFinder{iata: "jfk"},
+			request:  "?country=US&lat=43&lon=-70",
+			wantCode: http.StatusOK,
+			wantIata: "jfk",
+		},
+		{
+			name:     "no-country",
+			iata:     &fakeIataFinder{iata: "jfk"},
+			request:  "?lat=43&lon=-70",
+			wantCode: http.StatusBadRequest,
+		},
+		{
+			name:     "bad-lat-lon",
+			iata:     &fakeIataFinder{iata: "jfk"},
+			request:  "?country=US&lat=ten&lon=twelve",
+			wantCode: http.StatusBadRequest,
+		},
+		{
+			name:     "error-lookup",
+			iata:     &fakeIataFinder{err: errors.New("fake error")},
+			request:  "?country=US&lat=43&lon=-70",
+			wantCode: http.StatusInternalServerError,
+		},
+		{
+			name: "success-headers",
+			iata: &fakeIataFinder{iata: "jfk"},
+			headers: map[string]string{
+				"X-AppEngine-Country":     "US",
+				"X-AppEngine-CityLatLong": "43,-73",
+			},
+			wantCode: http.StatusOK,
+			wantIata: "jfk",
+		},
+		{
+			name: "error-bad-latlon-headers",
+			iata: &fakeIataFinder{iata: "jfk"},
+			headers: map[string]string{
+				"X-AppEngine-Country":     "US",
+				"X-AppEngine-CityLatLong": "xx,zz,yy",
+			},
+			wantCode: http.StatusBadRequest,
+		},
+		{
+			name: "error-unknown-latlon-headers",
+			iata: &fakeIataFinder{iata: "jfk"},
+			headers: map[string]string{
+				"X-AppEngine-Country":     "US",
+				"X-AppEngine-CityLatLong": "0.000000,0.000000",
+			},
+			wantCode: http.StatusBadRequest,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewServer(tt.project, tt.iata)
+			rw := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodGet, "/autojoin/v0/lookup"+tt.request, nil)
+			for key, value := range tt.headers {
+				req.Header.Set(key, value)
+			}
+			s.Lookup(rw, req)
+			if rw.Code != tt.wantCode {
+				t.Errorf("Lookup() returned wrong code; got %d, want %d", rw.Code, tt.wantCode)
+			}
+			resp := strings.Trim(rw.Body.String(), "\n")
+			if rw.Code == http.StatusOK && resp != tt.wantIata {
+				t.Errorf("Lookup() returned wrong iata; got %s, want %s", resp, tt.wantIata)
+			}
+		})
+	}
+}
+
+func TestServer_Reload(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		f := &fakeIataFinder{}
+		s := NewServer("fake", f)
+		s.Reload(context.Background())
+		if f.loads != 1 {
+			t.Errorf("Reload failed to call iata loader")
+		}
+	})
+}
