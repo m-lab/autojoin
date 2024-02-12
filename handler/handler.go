07616e4576dd67f1885c702712b9347005117b74
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-02-12 14:56:10 -0500

    simple handler for lookup

--- handler/handler.go
+++ handler/handler.go
@@ -1,25 +1,59 @@
 package handler
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 // Server maintains shared state for the server.
 type Server struct {
 	Project string
+	Iata    IataFinder
+}
+
+type IataFinder interface {
+	Lookup(country string, lat, lon float64) (string, error)
+	Load(ctx context.Context) error
 }
 
 // NewServer creates a new Server instance for request handling.
-func NewServer(project string) *Server {
+func NewServer(project string, finder IataFinder) *Server {
 	return &Server{
 		Project: project,
+		Iata:    finder,
 	}
+}
+
+func (s *Server) Reload(ctx context.Context) {
+	s.Iata.Load(ctx)
 }
 
 // Lookup is a handler used to find the nearest IATA given client IP or lat/lon metadata.
 func (s *Server) Lookup(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(rw, "TODO(soltesz): complete lookup logic\n")
+	country := rawCountry(req)
+	if country == "" {
+		rw.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(rw, "could not determine country")
+		return
+	}
+	rlat, rlon := rawLatLon(req)
+	lat, errLat := strconv.ParseFloat(rlat, 64)
+	lon, errLon := strconv.ParseFloat(rlon, 64)
+	if errLat != nil || errLon != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(rw, "could not determine lat/lon")
+		return
+	}
+	code, err := s.Iata.Lookup(country, lat, lon)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(rw, "could not determine iata code: %v", err)
+		return
+	}
+	fmt.Fprintf(rw, "%s\n", code)
 }
 
 // Register is a handler used by autonodes to register with M-Lab on startup.
@@ -35,4 +69,36 @@ func (s *Server) Live(rw http.ResponseWriter, req *http.Request) {
 // Ready reports whether the server is ready.
 func (s *Server) Ready(rw http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(rw, "ok")
+}
+
+func rawCountry(req *http.Request) string {
+	c := req.URL.Query().Get("country")
+	if c != "" {
+		return c
+	}
+	c = req.URL.Query().Get("X-AppEngine-Country")
+	if c != "" {
+		return c
+	}
+	// TODO: lookup with request IP.
+	return ""
+}
+
+func rawLatLon(req *http.Request) (string, string) {
+	lat := req.URL.Query().Get("lat")
+	lon := req.URL.Query().Get("lon")
+	if lat != "" && lon != "" {
+		return lat, lon
+	}
+	latlon := req.URL.Query().Get("X-AppEngine-CityLatLong")
+	if latlon == "0.000000,0.000000" {
+		// TODO: lookup with request IP.
+		return "", ""
+	}
+	fields := strings.Split(latlon, ",")
+	if len(fields) == 2 {
+		return fields[0], fields[1]
+	}
+	// TODO: lookup with request IP.
+	return "", ""
 }
