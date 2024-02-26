a39bbe579c4a3eb19f4cd624c10ffc6e7c3f73e5
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-02-26 16:12:42 -0500

    Add api definition for Lookup and Register responses (#7)
    * Add v0 api for LookupResponse and RegisterRespones

---
+++ api/v0/api.go
@@ -0,0 +1,48 @@
+package v0
+
+import (
+	v2 "github.com/m-lab/locate/api/v2"
+	"github.com/m-lab/uuid-annotator/annotator"
+)
+
+// LookupResponse is returned by a lookup request.
+type LookupResponse struct {
+	Error  *v2.Error `json:",omitempty"`
+	Lookup *Lookup   `json:",omitempty"`
+}
+
+// Lookup is returned for a successful lookup request.
+type Lookup struct {
+	IATA string
+}
+
+// RegisterResponse is returned by a register request.
+type RegisterResponse struct {
+	Error        *v2.Error     `json:",omitempty"`
+	Registration *Registration `json:",omitempty"`
+}
+
+// Network contains IPv4 and IPv6 addresses.
+type Network struct {
+	IPv4 string
+	IPv6 string
+}
+
+// ServerAnnotation is used by the uuid-annotator.
+// From: https://github.com/m-lab/uuid-annotator/blob/main/siteannotator/server.go#L83-L90
+type ServerAnnotation struct {
+	Annotation annotator.ServerAnnotations
+	Network    Network
+	Type       string
+}
+
+// Registration is returned for a successful registration request.
+type Registration struct {
+	// Hostname is the dynamic DNS name. Hostname should be available immediately.
+	Hostname string
+
+	// Annotation is the metadata used by the uuid-annotator for all server annotations.
+	Annotation *ServerAnnotation `json:",omitempty"`
+	// Heartbeat is the registration message used by the heartbeat service to register with the Locate API.
+	Heartbeat *v2.Registration `json:",omitempty"`
+}
