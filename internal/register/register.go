920afb1cbaab641d5b6643e3561ed6c0b0cb2c6a
Roberto D'Auria <roberto@measurementlab.net>
2024-07-10 22:30:56 +0200

    Fix v0.Network in register responses.

--- internal/register/register.go
+++ internal/register/register.go
@@ -65,6 +65,17 @@ func CreateRegisterResponse(p *Params) v0.RegisterResponse {
 		}
 	}
 
+	// A v0.Network must contain a valid CIDR, so we convert the v4/v6
+	// addresses to a /32 or a /64 here.
+	ipv4CIDR := ""
+	ipv6CIDR := ""
+	if p.IPv4 != "" {
+		ipv4CIDR = p.IPv4 + "/32"
+	}
+	if p.IPv6 != "" {
+		ipv6CIDR = p.IPv6 + "/64"
+	}
+
 	// Put everything together into a RegisterResponse.
 	r := v0.RegisterResponse{
 		Registration: &v0.Registration{
@@ -77,8 +88,8 @@ func CreateRegisterResponse(p *Params) v0.RegisterResponse {
 					Network: p.Network,
 				},
 				Network: v0.Network{
-					IPv4: p.IPv4,
-					IPv6: p.IPv6,
+					IPv4: ipv4CIDR,
+					IPv6: ipv6CIDR,
 				},
 				Type: "unknown", // should be overridden by node.
 			},
