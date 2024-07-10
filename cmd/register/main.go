70f62d791c4c3e59b175bd7ba4a7dde276905dc3
Roberto D'Auria <roberto@measurementlab.net>
2024-07-10 14:29:41 -0400

    Fix data type in the annotation.json file (#25)

--- cmd/register/main.go
+++ cmd/register/main.go
@@ -17,7 +17,6 @@ import (
 	"github.com/m-lab/go/memoryless"
 	"github.com/m-lab/go/rtx"
 	v2 "github.com/m-lab/locate/api/v2"
-	"github.com/m-lab/uuid-annotator/annotator"
 )
 
 const (
@@ -104,7 +103,7 @@ func register() {
 	}
 
 	heartbeat := map[string]v2.Registration{r.Registration.Hostname: *r.Registration.Heartbeat}
-	annotation := map[string]annotator.ServerAnnotations{r.Registration.Hostname: r.Registration.Annotation.Annotation}
+	annotation := map[string]v0.ServerAnnotation{r.Registration.Hostname: *r.Registration.Annotation}
 
 	// Write the hostname to a file.
 	err = os.WriteFile(path.Join(*outputPath, hostnameFilename), []byte(r.Registration.Hostname), 0644)
