1923fc54fbb93bfb2ec301172b1d842112e3f74f
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-09-09 15:58:16 -0400

    Include zone management operations in dnsiface

--- internal/dnsx/dnsiface/dns.go
+++ internal/dnsx/dnsiface/dns.go
@@ -10,11 +10,18 @@ import (
 type Service interface {
 	ResourceRecordSetsGet(ctx context.Context, project string, zone string, name string, type_ string) (*dns.ResourceRecordSet, error)
 	ChangeCreate(ctx context.Context, project string, zone string, change *dns.Change) (*dns.Change, error)
+	GetManagedZone(ctx context.Context, project, zoneName string) (*dns.ManagedZone, error)
+	CreateManagedZone(ctx context.Context, project string, z *dns.ManagedZone) (*dns.ManagedZone, error)
 }
 
 // CloudDNSService implements the DNS Service interface.
 type CloudDNSService struct {
 	Service *dns.Service
+}
+
+// NewCloudDNSService creates a new instance of the CloudDNSService.
+func NewCloudDNSService(s *dns.Service) *CloudDNSService {
+	return &CloudDNSService{Service: s}
 }
 
 // ResourceRecordSetsGet gets an existing resource record set, if present.
@@ -25,4 +32,14 @@ func (c *CloudDNSService) ResourceRecordSetsGet(ctx context.Context, project str
 // ChangeCreate applies the given change set.
 func (c *CloudDNSService) ChangeCreate(ctx context.Context, project string, zone string, change *dns.Change) (*dns.Change, error) {
 	return c.Service.Changes.Create(project, zone, change).Context(ctx).Do()
+}
+
+// GetManagedZone gets the named zone.
+func (c *CloudDNSService) GetManagedZone(ctx context.Context, project, zoneName string) (*dns.ManagedZone, error) {
+	return c.Service.ManagedZones.Get(project, zoneName).Context(ctx).Do()
+}
+
+// CreateManagedZone creates the given zone.
+func (c *CloudDNSService) CreateManagedZone(ctx context.Context, project string, zone *dns.ManagedZone) (*dns.ManagedZone, error) {
+	return c.Service.ManagedZones.Create(project, zone).Context(ctx).Do()
 }
