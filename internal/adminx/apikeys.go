2c184450a4b1aebd5cc7dc3b123886f129052b53
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-10-28 11:59:58 -0400

    Add docstrings

--- internal/adminx/apikeys.go
+++ internal/adminx/apikeys.go
@@ -7,17 +7,20 @@ import (
 	"github.com/googleapis/gax-go"
 )
 
+// KeysClient defines the interface used by the APIKeys type to allocate API keys.
 type KeysClient interface {
 	GetKeyString(ctx context.Context, req *apikeyspb.GetKeyStringRequest, opts ...gax.CallOption) (*apikeyspb.GetKeyStringResponse, error)
 	CreateKey(ctx context.Context, req *apikeyspb.CreateKeyRequest, opts ...gax.CallOption) (*apikeyspb.Key, error)
 }
 
+// APIKeys maintains state for allcoating API keys.
 type APIKeys struct {
 	locateProject string
 	client        KeysClient
 	namer         *Namer
 }
 
+// NewAPIKeys creates a new APIKeys instance for allocating API keys.
 func NewAPIKeys(locateProj string, c KeysClient, n *Namer) *APIKeys {
 	return &APIKeys{
 		locateProject: locateProj,
@@ -26,6 +29,8 @@ func NewAPIKeys(locateProj string, c KeysClient, n *Namer) *APIKeys {
 	}
 }
 
+// CreateKey returns an API key restricted to the Locate and Autojoin APIs for use by the named org.
+// CreateKey can be called multiple times safely.
 func (a *APIKeys) CreateKey(ctx context.Context, org string) (string, error) {
 	// Attempt to get the api key by name to see if it already exists.
 	get, err := a.client.GetKeyString(ctx, &apikeyspb.GetKeyStringRequest{
--- internal/adminx/keysiface/keys.go
+++ internal/adminx/keysiface/keys.go
@@ -12,16 +12,19 @@ type keysImpl struct {
 	client *apikeys.Client
 }
 
+// NewKeys creates an instance of the keys implementation.
 func NewKeys(k *apikeys.Client) *keysImpl {
 	return &keysImpl{
 		client: k,
 	}
 }
 
+// GetKeyString returns a struct containing the API key string.
 func (c *keysImpl) GetKeyString(ctx context.Context, req *apikeyspb.GetKeyStringRequest, opts ...gax.CallOption) (*apikeyspb.GetKeyStringResponse, error) {
 	return c.client.GetKeyString(ctx, req)
 }
 
+// CreateKey returns an API key based on the given request. CreateKey is a blocking request.
 func (c *keysImpl) CreateKey(ctx context.Context, req *apikeyspb.CreateKeyRequest, opts ...gax.CallOption) (*apikeyspb.Key, error) {
 	key, err := c.client.CreateKey(ctx, req)
 	if err != nil {
